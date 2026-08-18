75d90f0c135c34c0bbe2c4519d01efc75a9beb73
Amritanshu Mishra <mishraamritanshu15@gmail.com>
2026-08-18 19:48:02 +0530

    functions questions

---
+++ Functions/7_MaxOfThree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int maxOfThree(int a, int b, int c) {
+    if (a >= b && a >= c)
+        return a;
+    else if (b >= a && b >= c)
+        return b;
+    else
+        return c;
+}
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    cout << "Maximum number: " << maxOfThree(a, b, c) << endl;
+
+    return 0;
+}
