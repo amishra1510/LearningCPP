8ab5497054e37f26b0366dee7d2efee72a8e6789
Amritanshu <mishraamritanshu15@gmail.com>
2026-08-28 21:15:17 +0530

    Add power function example

---
+++ Functions/9_Power.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int power(int base, int exponent)
+{
+    int result = 1;
+
+    for (int i = 1; i <= exponent; i++)
+    {
+        result *= base;
+    }
+
+    return result;
+}
+
+int main()
+{
+    int base, exponent;
+
+    cout << "Enter base: ";
+    cin >> base;
+
+    cout << "Enter exponent: ";
+    cin >> exponent;
+
+    cout << "Result = " << power(base, exponent);
+
+    return 0;
+}
